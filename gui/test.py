d2263a14fee904cc1556ebf42d7295cc7eafc683
voldemort401 <181987124+voldemort401@users.noreply.github.com>
2026-02-06 22:45:11 +0545

    removed an unecessary file

--- gui/test.py
+++
@@ -1,6 +0,0 @@
-from nicegui import app, ui
-
-ui.textarea('This note is kept between visits').classes('w-full') \
-    .bind_value(app.storage.user, 'note')
-
-ui.run(storage_secret="asdiufjsadijfoiasdjfiosdajo")
