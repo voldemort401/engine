37654beadc9b0ab5615958eb85a9b8b8df5d7828
voldemort401 <181987124+voldemort401@users.noreply.github.com>
2026-02-14 12:54:57 +0545

    Implemented test for to_standardNOT

--- test/board/test.cpp
+++ test/board/test.cpp
@@ -1,5 +1,6 @@
 #include "../../engine/src/board/board.hpp"
 #include <bitset>
+#include <cmath>
 #include <string>
 #include <iostream>
    
@@ -13,44 +14,114 @@ class test {
 
 void test::test_to_bitboard(){
   piece result;
-  std::string square1{"e1"}; 
-  std::string square2{"h8"}; 
-  std::string square3{"20e"}; 
-  std::string square4{"e10"}; 
+  std::string square; 
 
-  result = to_bitboard(square4);
+  square = "20e";
+  result = to_bitboard(square);
   if (result != ENGINE_INVALID_SQUARE){
-    std::cout << "\033[31m" << "❌ Failed at "<< square4 << '\n';
+    std::cout << "\033[31m" << "❌ Failed at "<< square << '\n';
     std::cout << "Expected:"<<ENGINE_INVALID_SQUARE<<" Got:"<<result << "\033[0m"<<'\n';
   }
-
-  result = to_bitboard(square3);
-  if (result != ENGINE_INVALID_SQUARE){
-    std::cout << "\033[31m" << "❌ Failed at "<< square3 << '\n';
-    std::cout << "Expected:"<<ENGINE_INVALID_SQUARE<<" Got:"<<result << "\033[0m"<<'\n';
+  else{
+    std::cout<<"\033[32m"<<"✅ Passed! when square="<<square<<'\n';
   }
 
-  result = to_bitboard(square2);
+  square = "10";
+  result = to_bitboard(square);
+  if (result != ENGINE_INVALID_SQUARE){
+    std::cout << "\033[31m" << "❌ Failed at "<< square << '\n';
+    std::cout << "Expected:"<<ENGINE_INVALID_SQUARE<<" Got:"<<result << "\033[0m"<<'\n';
+  }
+  else{
+    std::cout<<"\033[32m"<<"✅ Passed! when square="<<square<<'\n';
+  }
+
+  square = "h8";
+  result = to_bitboard(square);
   if (result != 128){
     std::bitset<64> expected(64);
     std::bitset<64> got(result);
 
-    std::cout << "\033[31m" << "❌ Failed at "<< square2 << '\n';
+    std::cout << "\033[31m" << "❌ Failed at "<< square << '\n';
     std::cout << "Expected:"<<expected<<" Got:"<<got<< "\033[0m"<<'\n';
   }
+  else{
+    std::cout<<"\033[32m"<<"✅ Passed! when square="<<square<<'\n';
+  }
 
-  result = to_bitboard(square1);
+  square = "e1";
+  result = to_bitboard(square);
   if (result != 1152921504606846976){
     std::bitset<64>expected(1152921504606846976); 
     std::bitset<64>got(result);
 
-    std::cout << "\033[31m" << "❌ Failed at "<< square1 << '\n';
+    std::cout << "\033[31m" << "❌ Failed at "<< square << '\n';
     std::cout << "Expected:"<<expected<<" Got:"<<got<< "\033[0m"<<'\n';
+  }
+  else{
+    std::cout<<"\033[32m"<<"✅ Passed! when square="<<square<<'\n';
   }
 
 };
 
+void test::test_to_standardNOT(){
+  piece square;
+  std::string result, expected; 
+
+  square = 1152921504606846976;
+  result = to_standardNOT(square);
+  expected = "e1";
+  if (result != expected){
+    std::bitset<64> sqr(square);
+    std::cout<<"\033[31m"<<"❌ Failed at" << sqr <<'\n';
+    std::cout<<"Expected"<<expected<<" Got:"<<result<<'\n';
+  }
+  else{
+    std::cout<<"\033[32m"<<"✅ Passed! when square="<<square<<'\n';
+  }
+
+  square = 68719476736;
+  result = to_standardNOT(square);
+  expected = "e4";
+  if (result != expected){
+    std::bitset<64> sqr(square);
+    std::cout<<"\033[31m"<<"❌ Failed at" << sqr <<'\n';
+    std::cout<<"Expected"<<expected<<" Got:"<<result<<'\n';
+  }
+  else{
+    std::cout<<"\033[32m"<<"✅ Passed! when square="<<square<<'\n';
+  }
+
+  square = 128;
+  result = to_standardNOT(square);
+  expected = "h8";
+  if (result != expected){
+    std::bitset<64> sqr(square);
+    std::cout<<"\033[31m"<<"❌ Failed at" << sqr <<'\n';
+    std::cout<<"Expected"<<expected<<" Got:"<<result<<'\n';
+  }
+  else{
+    std::cout<<"\033[32m"<<"✅ Passed! when square="<<square<<'\n';
+  }
+
+  square = 1;
+  result = to_standardNOT(square);
+  expected = "a8";
+  if (result != expected){
+    std::bitset<64> sqr(square);
+    std::cout<<"\033[31m"<<"❌ Failed at" << sqr <<'\n';
+    std::cout<<"Expected"<<expected<<" Got:"<<result<<'\n';
+  }
+  else{
+    std::cout<<"\033[32m"<<"✅ Passed! when square="<<square<<'\n';
+  }
+
+
+}
+
+
 int main(){
   test test; 
   test.test_to_bitboard();
+  test.test_to_standardNOT();
 };
